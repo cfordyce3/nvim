297f1125d998f8ac9928ce868414a8fdacd5af89
Chas Fordyce <charlesfordyce3@gmail.com>
2023-03-02 16:55:34 +0100

    Set NVim-Tree to startup on directory input

--- lua/chas/plugins/nvim-tree.lua
+++ lua/chas/plugins/nvim-tree.lua
@@ -6,6 +6,26 @@ end
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function open_nvim_tree(data)
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+        return
+    end
+
+    -- new empty buffer
+    vim.cmd.enew()
+
+    -- wipe the directory buffer
+    vim.cmd.bw(data.buf)
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
 nvimtree.setup({
     actions = {
         open_file = {
@@ -15,3 +35,5 @@ nvimtree.setup({
         },
     },
 })
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
